0f64686aac1f7884455eccf2a9170a7355420f63
Fon <kemmler00@gmail.com>
2025-09-28 15:49:44 +0200

    intento de marron

--- webmijillasoft/lib/screens/ProyectoKitt.dart
+++ webmijillasoft/lib/screens/ProyectoKitt.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:webmijillasoft/widgets/footer.dart';
+import 'package:webmijillasoft/widgets/top_bar.dart';
 
 class ProyectoKitt extends StatelessWidget {
   const ProyectoKitt({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const MijillaSoftAppBar(),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.car_crash, // Un icono de ejemplo
+              size: 100.0,
+              color: Color.fromRGBO(184, 212, 166, 0.83),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '¡Proyecto K.I.T.T!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Proximamente actualizaremos.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const MijillaSoftFooter(),
+    );
   }
 }
